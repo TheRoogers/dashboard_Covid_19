338d163622ed6b83b9649c7a412443dde4faa880
theroogers <theroogers@gmail.com>
2021-05-07 19:16:17 -0300

    adicionado dicas de prevenção

--- files/dashboard_covid_19/lib/data/data.dart
+++ files/dashboard_covid_19/lib/data/data.dart
@@ -1,7 +1,7 @@
 final prevention = [
-  {'assets/images/distance.png': 'Avoid close\ncontact'},
-  {'assets/images/wash_hands.png': 'Clean your\nhands often'},
-  {'assets/images/mask.png': 'Wear a\nfacemask'},
+  {'assets/images/distance.png': 'Evite contato\npróximo'},
+  {'assets/images/wash_hands.png': 'Lave suas mãos\nfrequentemente'},
+  {'assets/images/mask.png': 'Use\nmáscara'},
 ];
 
 final covidUSADailyNewCases = [12.17, 11.15, 10.02, 11.21, 13,83, 14.16, 14.30];
--- files/dashboard_covid_19/lib/screens/home_screen.dart
+++ files/dashboard_covid_19/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:dashboard_covid_19/config/palette.dart';
+import 'package:dashboard_covid_19/config/styles.dart';
+import 'package:dashboard_covid_19/data/data.dart';
 import 'package:flutter/material.dart';
 import 'package:dashboard_covid_19/widgets/widgets.dart';
 
@@ -9,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String _country = 'BR';
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -18,6 +21,8 @@ class _HomeScreenState extends State<HomeScreen> {
         physics: ClampingScrollPhysics(),
         slivers: <Widget>[
           _buildHeader(screenHeight),
+          _buildPreventionTips(screenHeight),
+          _buildYourOwnTest(screenHeight),
         ],
       ),
     );
@@ -28,16 +33,16 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Container(
         padding: const EdgeInsets.all(20.0),
         decoration: BoxDecoration(
-          color: Palette.primaryColor,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(40.0),
-            bottomRight: Radius.circular(40.0),
-          )
-        ),
+            color: Palette.primaryColor,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(40.0),
+              bottomRight: Radius.circular(40.0),
+            )),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
                   'COVID-19',
@@ -48,12 +53,146 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 CountryDropdown(
-                  countries: ['BR', 'CN', 'FR', 'IN','IT', 'UK', 'USA'],
+                  countries: ['BR', 'CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
                   country: _country,
-                  onChanged: (val) => setState(()=> _country = val),
+                  onChanged: (val) => setState(() => _country = val),
                 ),
-              ],)
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.03),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Você está se sentindo mal?',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Text(
+                  'Se você estiver com qualquer sintoma de Covid-19, ligue ou envie uma mensagem imediatamente para obter ajuda ',
+                  style: const TextStyle(
+                    color: Colors.white70,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.03),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton.icon(
+                      // icone de ligação
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      onPressed: () {},
+                      color: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      icon: const Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        'Ligue Agora',
+                        style: Styles.buttonTextStyle,
+                      ),
+                      textColor: Colors.white,
+                    ),
+                    FlatButton.icon(
+                      //icone de mensagem
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      onPressed: () {},
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      icon: const Icon(
+                        Icons.chat_bubble,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        'Enviar SMS',
+                        style: Styles.buttonTextStyle,
+                      ),
+                      textColor: Colors.white,
+                    ),
+                  ],
+                )
+              ],
+            )
           ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildPreventionTips(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Dicas de Prevenção',
+              style: const TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: prevention
+                  .map((e) => Column(
+                        children: <Widget>[
+                          Image.asset(
+                            e.keys.first,
+                            height: screenHeight * 0.12,
+                          ),
+                          SizedBox(height: screenHeight * 0.015),
+                          Text(
+                            e.values.first,
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildYourOwnTest(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 20.0,
+        ),
+        padding: const EdgeInsets.all(10.0),
+        height: screenHeight * 0.15,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFAD9FE4), Palette.primaryColor],
+          ),
         ),
       ),
     );
